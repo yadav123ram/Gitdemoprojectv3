1a078e7390bbcd2c6fc4d61c5643a4716f6b6772
ramsingaryadav <151905673+ramsingaryadav@users.noreply.github.com>
2023-12-06 22:03:15 +0530

    Added additional checks.......

--- src/Gitproject/Test2.java
+++ src/Gitproject/Test2.java
@@ -6,7 +6,7 @@ public class Test2 {
 		String hungry="yes";
 		
 		
-		if(hungry=="yes") {
+		if(hungry=="yes"||hungry=="Y"||hungry=="y") {
 			System.out.println("eat something......");
 			
 		}else {
